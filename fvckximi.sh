d8db55e82a458bd888b625e89bcee27a33351015
ndtai1527 <124653651+ndtai1527@users.noreply.github.com>
2024-06-17 21:02:43 +0700

    Update fvckximi.sh

--- fvckximi.sh
+++ fvckximi.sh
@@ -5,69 +5,102 @@ repS="python3 $dir/bin/strRep.py"
 apktool="java -jar $dir/bin/apktool.jar"
 echo "Starting script in directory: $dir"
 
-apk_util() {
-    cd $dir
-    echo "Inside apk_util, current directory: $(pwd)"
-    
-    if [[ $1 == "d" ]]; then
-        echo -ne "====> Patching $2 : "
-        if [[ -f $dir/services.jar ]]; then
-            sudo cp $dir/services.jar $dir/jar_temp
-            sudo chown $(whoami) $dir/jar_temp/$2
-            $apktool d $dir/jar_temp/$2 -o $dir/jar_temp/$2.out
-            if [[ -d $dir/jar_temp/"$2.out" ]]; then
-                rm -rf $dir/jar_temp/$2
-            fi
-        fi
-    else 
-        if [[ $1 == "a" ]]; then 
-            if [[ -d $dir/jar_temp/$2.out ]]; then
-                cd $dir/jar_temp/$2.out
-                echo "Inside apk_util (assemble), current directory: $(pwd)"
-                $apktool b $dir/jar_temp/$2.out -o $dir/jar_temp/$2
-                if [[ -f $dir/jar_temp/$2 ]]; then
-                    sudo cp -rf $dir/jar_temp/$2 $dir/module/system/framework
-                    final_dir="$dir/module/*"
-                    echo "Success"
-                    rm -rf $dir/jar_temp/$2.out
-                else
-                    echo "Fail"
-                fi
-            fi
-        fi
-    fi
+jar_util() 
+{
+	cd $dir
+	#binary
+	if [[ $3 == "fw" ]]; then 
+		bak="java -jar $dir/bin/baksmali.jar d"
+		sma="java -jar $dir/bin/smali.jar a"
+	else
+		bak="java -jar $dir/bin/baksmali-2.5.2.jar d"
+		sma="java -jar $dir/bin/smali-2.5.2.jar a"
+	fi
+
+	if [[ $1 == "d" ]]; then
+		echo -ne "====> Patching $2 : "
+		if [[ -f $dir/miui-services.jar ]]; then
+			sudo cp $dir/miui-services.jar $dir/jar_temp
+			sudo chown $(whoami) $dir/jar_temp/$2
+			unzip $dir/jar_temp/$2 -d $dir/jar_temp/$2.out  >/dev/null 2>&1
+			if [[ -d $dir/jar_temp/"$2.out" ]]; then
+				rm -rf $dir/jar_temp/$2
+				for dex in $(find $dir/jar_temp/"$2.out" -maxdepth 1 -name "*dex" ); do
+						if [[ $4 ]]; then
+							if [[ ! "$dex" == *"$4"* ]]; then
+								$bak $dex -o "$dex.out"
+								[[ -d "$dex.out" ]] && rm -rf $dex
+							fi
+						else
+							$bak $dex -o "$dex.out"
+							[[ -d "$dex.out" ]] && rm -rf $dex		
+						fi
+
+				done
+			fi
+		fi
+	else 
+		if [[ $1 == "a" ]]; then 
+			if [[ -d $dir/jar_temp/$2.out ]]; then
+				cd $dir/jar_temp/$2.out
+				for fld in $(find -maxdepth 1 -name "*.out" ); do
+					if [[ $4 ]]; then
+						if [[ ! "$fld" == *"$4"* ]]; then
+							$sma $fld -o $(echo ${fld//.out})
+							[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld
+						fi
+					else 
+						$sma $fld -o $(echo ${fld//.out})
+						[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld	
+					fi
+				done
+				7za a -tzip -mx=0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/. >/dev/null 2>&1
+				#zip -r -j -0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/.
+				zipalign 4 $dir/jar_temp/$2_notal $dir/jar_temp/$2
+				if [[ -f $dir/jar_temp/$2 ]]; then
+					sudo cp -rf $dir/jar_temp/$2 $dir/module/system/system_ext/miui-services.jar
+					final_dir="$dir/module/*"
+					#7za a -tzip "$dir/miui-services_patched_$(date "+%d%m%y").zip" $final_dir
+					echo "Success"
+					rm -rf $dir/jar_temp/$2.out $dir/jar_temp/$2_notal 
+				else
+					echo "Fail"
+				fi
+			fi
+		fi
+	fi
 }
 
 services() {
     lang_dir="$dir/module/lang"
-    apk_util d "services.jar"
+    jar_util d "services.jar"
 
-    s0=$(find $dir/jar_temp/services.jar.out -name "PermissionManagerServiceImpl.smali")
+    s0=$(find -name "PermissionManagerServiceImpl.smali")
     [[ -f $s0 ]] && $repS $dir/signature/PermissionManagerServiceImpl/updatePermissionFlags.config.ini $s0
     [[ -f $s0 ]] && $repS $dir/signature/PermissionManagerServiceImpl/shouldGrantPermissionBySignature.config.ini $s0
     [[ -f $s0 ]] && $repS $dir/signature/PermissionManagerServiceImpl/revokeRuntimePermissionNotKill.config.ini $s0
     [[ -f $s0 ]] && $repS $dir/signature/PermissionManagerServiceImpl/revokeRuntimePermission.config.ini $s0
     [[ -f $s0 ]] && $repS $dir/signature/PermissionManagerServiceImpl/grantRuntimePermission.config.ini $s0
 
-    s1=$(find $dir/jar_temp/services.jar.out -name "PermissionManagerServiceStub.smali")
+    s1=$(find -name "PermissionManagerServiceStub.smali")
     [[ -f $s1 ]] && echo $(cat $dir/signature/PermissionManagerServiceStub/onAppPermFlagsModified.config.ini) >> $s1
     
-    s2=$(find $dir/jar_temp/services.jar.out -name "ParsingPackageUtils.smali")
+    s2=$(find -name "ParsingPackageUtils.smali")
     [[ -f $s2 ]] && $repS $dir/signature/ParsingPackageUtils/getSigningDetails.config.ini $s2
 
-    s3=$(find $dir/jar_temp/services.jar.out -name 'PackageManagerService$PackageManagerInternalImpl.smali')
+    s3=$(find -name 'PackageManagerService$PackageManagerInternalImpl.smali')
     [[ -f $s3 ]] && $repS $dir/signature/'PackageManagerService$PackageManagerInternalImpl'/isPlatformSigned.config.ini $s3
 
-    s4=$(find $dir/jar_temp/services.jar.out -name "PackageManagerServiceUtils.smali")
+    s4=$(find -name "PackageManagerServiceUtils.smali")
     [[ -f $s4 ]] && $repS $dir/signature/PackageManagerServiceUtils/verifySignatures.config.ini $s4
 
-    s5=$(find $dir/jar_temp/services.jar.out -name "ReconcilePackageUtils.smali")
+    s5=$(find -name "ReconcilePackageUtils.smali")
     [[ -f $s5 ]] && $repS $dir/signature/ReconcilePackageUtils/reconcilePackages.config.ini $s5
 
-    s6=$(find $dir/jar_temp/services.jar.out -name "ScanPackageUtils.smali")
+    s6=$(find -name "ScanPackageUtils.smali")
     [[ -f $s6 ]] && $repS $dir/signature/ScanPackageUtils/assertMinSignatureSchemeIsValid.config.ini $s6
 
-    apk_util a "services.jar"
+    jar_util a "services.jar"
 }
 
 if [[ ! -d $dir/jar_temp ]]; then
