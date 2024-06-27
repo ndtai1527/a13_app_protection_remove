525fc83cf88de8b532255a5c75555f150a4b54f4
ndtai1527 <ductai1527@gmail.com>
2024-06-27 21:53:46 +0700

    fvck

--- fvckximi.sh
+++ fvckximi.sh
@@ -4,63 +4,67 @@ repS="python3 $dir/bin/strRep.py"
 tmp_dir="$dir/smali_tmp"
 jar_util() 
 {
-    cd $dir
-    #binary
-    if [[ $3 == "fw" ]]; then 
-        bak="java -jar $dir/bin/baksmali.jar d"
-        sma="java -jar $dir/bin/smali.jar a"
-    fi
+	cd $dir
+	#binary
+	if [[ $3 == "fw" ]]; then 
+		bak="java -jar $dir/bin/baksmali.jar d"
+		sma="java -jar $dir/bin/smali.jar a"
+	fi
 
-    if [[ $1 == "d" ]]; then
-        echo -ne "====> Patching $2 : "
-        if [[ -f $dir/services.jar ]]; then
-            sudo cp $dir/services.jar $dir/jar_temp
-            sudo chown $(whoami) $dir/jar_temp/$2
-            unzip $dir/jar_temp/$2 -d $dir/jar_temp/$2.out  >/dev/null 2>&1
-            if [[ -d $dir/jar_temp/"$2.out" ]]; then
-                rm -rf $dir/jar_temp/$2
-                for dex in $(find $dir/jar_temp/"$2.out" -maxdepth 1 -name "*dex" ); do
-                    if [[ $4 ]]; then
-                        if [[ ! "$dex" == *"$4"* ]]; then
-                            $bak $dex -o "$dex.out"
-                            [[ -d "$dex.out" ]] && rm -rf $dex
-                        fi
-                    else
-                        $bak $dex -o "$dex.out"
-                        [[ -d "$dex.out" ]] && rm -rf $dex        
-                    fi
-                done
-            fi
-        fi
-    else 
-        if [[ $1 == "a" ]]; then 
-            if [[ -d $dir/jar_temp/$2.out ]]; then
-                cd $dir/jar_temp/$2.out
-                for fld in $(find -maxdepth 1 -name "*.out" ); do
-                    if [[ $4 ]]; then
-                        if [[ ! "$fld" == *"$4"* ]]; then
-                            $sma $fld -o $(echo ${fld//.out})
-                            [[ -f $(echo ${fld//.out}) ]] && rm -rf $fld
-                        fi
-                    else 
-                        $sma $fld -o $(echo ${fld//.out})
-                        [[ -f $(echo ${fld//.out}) ]] && rm -rf $fld    
-                    fi
-                done
-                7za a -tzip -mx=0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/. >/dev/null 2>&1
-                zipalign 4 $dir/jar_temp/$2_notal $dir/jar_temp/$2
-                if [[ -f $dir/jar_temp/$2 ]]; then
-                    sudo cp -rf $dir/jar_temp/$2 $dir/module/system/framework
-                    final_dir="$dir/module/*"
-                    echo "Success"
-                    rm -rf $dir/jar_temp/$2.out $dir/jar_temp/$2_notal 
-                else
-                    echo "Fail"
-                fi
-            fi
-        fi
-    fi
+	if [[ $1 == "d" ]]; then
+		echo -ne "====> Patching $2 : "
+		if [[ -f $dir/services.jar ]]; then
+			sudo cp $dir/services.jar $dir/jar_temp
+			sudo chown $(whoami) $dir/jar_temp/$2
+			unzip $dir/jar_temp/$2 -d $dir/jar_temp/$2.out  >/dev/null 2>&1
+			if [[ -d $dir/jar_temp/"$2.out" ]]; then
+				rm -rf $dir/jar_temp/$2
+				for dex in $(find $dir/jar_temp/"$2.out" -maxdepth 1 -name "*dex" ); do
+						if [[ $4 ]]; then
+							if [[ ! "$dex" == *"$4"* ]]; then
+								$bak $dex -o "$dex.out"
+								[[ -d "$dex.out" ]] && rm -rf $dex
+							fi
+						else
+							$bak $dex -o "$dex.out"
+							[[ -d "$dex.out" ]] && rm -rf $dex		
+						fi
+
+				done
+			fi
+		fi
+	else 
+		if [[ $1 == "a" ]]; then 
+			if [[ -d $dir/jar_temp/$2.out ]]; then
+				cd $dir/jar_temp/$2.out
+				for fld in $(find -maxdepth 1 -name "*.out" ); do
+					if [[ $4 ]]; then
+						if [[ ! "$fld" == *"$4"* ]]; then
+							$sma $fld -o $(echo ${fld//.out})
+							[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld
+						fi
+					else 
+						$sma $fld -o $(echo ${fld//.out})
+						[[ -f $(echo ${fld//.out}) ]] && rm -rf $fld	
+					fi
+				done
+				7za a -tzip -mx=0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/. >/dev/null 2>&1
+				#zip -r -j -0 $dir/jar_temp/$2_notal $dir/jar_temp/$2.out/.
+				zipalign 4 $dir/jar_temp/$2_notal $dir/jar_temp/$2
+				if [[ -f $dir/jar_temp/$2 ]]; then
+					sudo cp -rf $dir/jar_temp/$2 $dir/module/system/framework
+					final_dir="$dir/module/*"
+					#7za a -tzip "$dir/services_patched_$(date "+%d%m%y").zip" $final_dir
+					echo "Success"
+					rm -rf $dir/jar_temp/$2.out $dir/jar_temp/$2_notal 
+				else
+					echo "Fail"
+				fi
+			fi
+		fi
+	fi
 }
+
 
 services() {
     jar_util d "services.jar" fw
@@ -69,14 +73,13 @@ services() {
     [[ ! -d $tmp_dir ]] && mkdir $tmp_dir
 
     # Files to be patched
-    files=("PermissionManagerServiceImpl" "PermissionManagerServiceStub" "ParsingPackageUtils"
-           "PackageManagerService\$PackageManagerInternalImpl" "PackageManagerServiceUtils"
-           "ReconcilePackageUtils" "ScanPackageUtils")
+    files=("PermissionManagerServiceImpl.smali" "PermissionManagerServiceStub.smali" "ParsingPackageUtils.smali"
+           "PackageManagerService\$PackageManagerInternalImpl.smali" "PackageManagerServiceUtils.smali"
+           "ReconcilePackageUtils.smali" "ScanPackageUtils.smali")
 
-    echo "Checking for files in $dir/jar_temp/services.jar.out"
     # Find and copy the files to the temporary directory
     for file in "${files[@]}"; do
-        src_file=$(find $dir/jar_temp/services.jar.out -maxdepth 1 -name "$file")
+        src_file=$(find $dir/jar_temp/services.jar.out -name "$file")
         if [[ -f $src_file ]]; then
             cp $src_file $tmp_dir/
             echo "Copied $src_file to $tmp_dir"
@@ -125,9 +128,11 @@ services() {
 
     jar_util a "services.jar" fw  
 }
-
 if [[ ! -d $dir/jar_temp ]]; then
-    mkdir $dir/jar_temp
+
+	mkdir $dir/jar_temp
+	
 fi
 
 services
+
